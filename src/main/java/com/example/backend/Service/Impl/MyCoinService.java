beb13ac7313327852169c27bf4dd0adabcf6b2ef
zongheng <181870301@smail.nju.edu.cn>
2021-05-25 22:11:12 +0800

    feat: 将酒店信息转化为图谱

--- src/main/java/com/example/backend/Service/Impl/MyCoinService.java
+++ src/main/java/com/example/backend/Service/Impl/MyCoinService.java
@@ -1,15 +1,13 @@
 package com.example.backend.Service.Impl;
 
 import com.example.backend.Exception.ResourceNotFoundException;
-import com.example.backend.Model.KG;
-import com.example.backend.Model.Link;
-import com.example.backend.Model.Node;
-import com.example.backend.Repository.LinkRepository;
-import com.example.backend.Repository.NodeRepository;
+import com.example.backend.Model.*;
+import com.example.backend.Repository.*;
 import com.example.backend.Service.IMyCoinService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -20,6 +18,15 @@ public class MyCoinService implements IMyCoinService {
 
     @Autowired
     private LinkRepository linkRepository;
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private RankRepository rankRepository;
 
     @Override
     public boolean addNode(Node node) {
@@ -80,8 +87,56 @@ public class MyCoinService implements IMyCoinService {
     @Override
     public HashMap<String, Object> getCoin() {
         HashMap<String, Object> coin = new HashMap<>();
-        coin.put("node", nodeRepository.findAll());
-        coin.put("relationship", linkRepository.findAll());
+        List<Object> nodeList = new ArrayList<>();
+        List<Object> linkList = new ArrayList<>();
+
+        for (Group group : groupRepository.findAll()) {
+            HashMap<String, Object> currNode = new HashMap<>();
+            currNode.put("name", group.getName());
+            currNode.put("uuid", "group" + group.getId());
+            currNode.put("type", "Group");
+            currNode.put("color", "rgb(125,213,255)");
+            currNode.put("shape", "diamond");
+            nodeList.add(currNode);
+        }
+
+        for (Brand brand : brandRepository.findAll()) {
+            HashMap<String, Object> currNode = new HashMap<>();
+            currNode.put("name", brand.getName());
+            currNode.put("uuid", "brand" + brand.getId());
+            currNode.put("type", "Brand");
+            currNode.put("color", "rgb(112,211,189)");
+            currNode.put("shape", "circle");
+            nodeList.add(currNode);
+
+            HashMap<String, Object> currLink1 = new HashMap<>();
+            currLink1.put("sourceid", "group" + brand.getGid());
+            currLink1.put("targetid", "brand" + brand.getId());
+            currLink1.put("name", "从属于");
+            currLink1.put("uuid", "group" + brand.getGid() + "-" + "brand" + brand.getId());
+            linkList.add(currLink1);
+
+            HashMap<String, Object> currLink2 = new HashMap<>();
+            currLink2.put("sourceid", "rank" + brand.getRid());
+            currLink2.put("targetid", "brand" + brand.getId());
+            currLink2.put("name", "等级");
+            currLink2.put("uuid", "rank" + brand.getRid() + "-" + "brand" + brand.getId());
+            linkList.add(currLink2);
+        }
+
+        for (Rank rank : rankRepository.findAll()) {
+            HashMap<String, Object> currNode = new HashMap<>();
+            currNode.put("name", rank.getName());
+            currNode.put("uuid", "rank" + rank.getId());
+            currNode.put("type", "Rank");
+            currNode.put("color", "rgb(255,131,115)");
+            currNode.put("shape", "square");
+            nodeList.add(currNode);
+        }
+
+
+        coin.put("nodes", nodeList);
+        coin.put("links", linkList);
         return coin;
     }
 

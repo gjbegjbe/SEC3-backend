b4a813ed57d0a3664af3d77760fed97a901fb9d9
zongheng <181870301@smail.nju.edu.cn>
2021-06-14 23:04:30 +0800

    feat: 添加了redis缓存

--- src/main/java/com/example/backend/Model/Brand.java
+++ src/main/java/com/example/backend/Model/Brand.java
@@ -4,9 +4,11 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
 
 @Document(collection = "Brand")
-public class Brand {
+public class Brand implements Serializable {
 
     @Transient
     public static final String SEQUENCE_NAME = "brands_sequence";
--- src/main/java/com/example/backend/Repository/BrandRepository.java
+++ src/main/java/com/example/backend/Repository/BrandRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.Repository;
 
 import com.example.backend.Model.Brand;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,13 +12,18 @@ public interface BrandRepository extends MongoRepository<Brand, Long> {
 
     void deleteByGid(Long gid);
 
+    @Cacheable(value = "brand", unless = "#result == null")
     List<Brand> findAllByGid(long gid);
 
+    @Cacheable(value = "brand", unless = "#result == null")
     Brand findByName(String name);
 
+    @Cacheable(value = "brand", unless = "#result == null")
     Brand findByNameContains(String name);
 
+    @Cacheable(value = "brand", unless = "#result == null")
     List<Brand> findTop8ByRidAndPriority(long rid, String priority);
 
+    @Cacheable(value = "brand", unless = "#result == null")
     List<Brand> findTop8ByGidAndRidAndPriority(long gid, long rid, String priority);
 }

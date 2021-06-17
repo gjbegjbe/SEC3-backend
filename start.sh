054087df4c822659c4ff2a783f14bb17b8c1df6a
zongheng <181870301@smail.nju.edu.cn>
2021-06-18 00:01:21 +0800

    ci: 修改了sh文件

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 echo "Starting SpringBoot Application"
 pwd
 export JENKINS_NODE_COOKIE=dontKillMe
-nohup java -Dhudson.util.ProcessTree.disable=true -jar target/*.jar --server.port=8081
+nohup java -Dhudson.util.ProcessTree.disable=true -jar target/*.jar --server.port=8081 &
